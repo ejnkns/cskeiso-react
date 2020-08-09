7b5c3b3fe7cec4ae722200f86310373041f62417
Elliot Jenkins <elliotjenkins3@gmail.com>
2020-08-09 15:23:36 +1000

    videos fade in

--- client/src/common/Player.js
+++ client/src/common/Player.js
@@ -1,6 +1,5 @@
-import React, { useEffect, Suspense } from 'react';
+import React, { useEffect, Suspense, useRef } from 'react';
 import '../App.css';
-import ReactPlayer from 'react-player';
 /*
 const homePlaceholder = React.lazy(() =>
 import('./sibhplaceholder.JPG'));
@@ -8,9 +7,10 @@ import('./sibhplaceholder.JPG'));
 /* i think i actually don't want this lazy loaded.
     they're almost alwqays immediatly in the view port
     I want the videos to be viewable as soon as possible
+
+    */
 const ReactPlayer = React.lazy(() =>
 import('react-player'));
-*/
 
 function Player({url}) {
     // after some testing it seems like videos only take marginally longer 
@@ -21,49 +21,53 @@ function Player({url}) {
             <div className="center-child">Loading...</div>
         </div>;
 
-
-
-
-
     // fade-in videos 
     useEffect(() => {
-        let els = document.querySelectorAll('.fade-in');
-        els.forEach((el) => el.classList.add('solid'));
     });
 
-    let player = null;
+    let playerRef = useRef(null);
     function handleReady() {
-        player = getInternalPlayer() // Internal player now ready
+        if (playerRef.current.getInternalPlayer()) {
+            // fade in the player
+            let playerEls = document.querySelectorAll('.fade-in');
+            playerEls.forEach((el) => el.classList.add('solid'));
+            // fade out the border
+            let borderEls = document.querySelectorAll('.border');
+            borderEls.forEach((el) => el.classList.add('border-fade-out'));
+        }
     }
 
     return (
-        <div className="player fade-in">
-            <Suspense fallback={fallback_div}>
-                <ReactPlayer
-                    onReady={handleReady}
-                    className='react-player'
-                    url = {url}
-                    width = '100%'
-                    height = '100%'
-                    config={{
-                        vimeo: {
-                            playerOptions: {
-                                title: 1,
-                                controls: 1
+        <div className="border">
+            <div className="player fade-in">
+                <Suspense fallback={fallback_div}>
+                    <ReactPlayer
+                        ref={playerRef}
+                        onReady={handleReady}
+                        className='react-player'
+                        url = {url}
+                        width = '100%'
+                        height = '100%'
+                        config={{
+                            vimeo: {
+                                playerOptions: {
+                                    title: 1,
+                                    controls: 1
+                                }
+                            },
+                            file: {
+                                attributes: {
+                                    autoPlay: true,
+                                    muted: true,
+                                    controls: false,
+                                    loop: true
+                                }
                             }
-                        },
-                        file: {
-                            attributes: {
-                                autoPlay: true,
-                                muted: true,
-                                controls: false,
-                                loop: true
-                            }
-                        }
 
-                    }}
-                />
-           </Suspense> 
+                        }}
+                    />
+                </Suspense> 
+            </div>
         </div>
     ) 
 }
