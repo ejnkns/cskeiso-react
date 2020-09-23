4d2d9c9832f1a914e76b9099130ad77386babe4f
Elliot Jenkins <elliotjenkins3@gmail.com>
2020-09-23 12:22:33 +1000

    improved footer

--- client/src/common/Footer.tsx
+++ client/src/common/Footer.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../App.css";
 
 function Footer() {
+  // repo https://github.com/cskeiso/cskeiso.github.io created dec 8 2018
   return (
     <div className="footer">
+      &copy; 2018 - {(new Date().getFullYear())} Carmen-Sibha Keiso<br/>
       Website created by
       <a target="_blank" rel="noopener noreferrer" href="https://ejnkns.com">
-        {" "}
-        Elliot Jenkins
+        &nbsp;Elliot Jenkins
       </a>
     </div>
   );
