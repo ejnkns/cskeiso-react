60530fc3efbdc590bad22550c4a51f254ba2b640
ejnkns <elliotjenkins3@gmail.com>
2020-10-15 12:46:56 +1100

    cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,21 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-/*
-import { getSheetRows } from "./common/GoogleSheets"
-import { GoogleSpreadsheetRow } from "google-spreadsheet";
-const CskeisoSpreadSheetID = "1O2V8Ms0aF0tGaijfQEDeU7r21aC_jBqq_B0deGxDarA"
-const FiveWaysSheetID = "0";
-// load in latest data from spreadsheet
-async function handleSheet() {
-  let rows: GoogleSpreadsheetRow[] | undefined = await getSheetRows(CskeisoSpreadSheetID, FiveWaysSheetID);
-  if (rows) {
-      for (let i = 0; i < rows.length; i++) {
-      }
-  }
-}
-//handleSheet()
-*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
